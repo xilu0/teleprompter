ca7f738c7ac862159317cfc8d9959b3068d9b948
yijie wang <785471184@qq.com>
2023-04-02 00:48:07 +0800

    update: init project

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Teleprompter
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+    }
+}
